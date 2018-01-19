8c914aa0090da2f8a742cefb361d3dc3d6c87aba
askabulkahfi <askabulkahfi1@gmail.com>
2018-01-19 21:53:52 +0800

    jenis harga layangan

--- Untitled4.cpp
+++ Untitled4.cpp
@@ -4,6 +4,17 @@ using namespace std;
 class layangan{
 	public:
 		void bahan(string bambu);
-		string merk;
+		string jenis;
 		double harga;
 };
+int main () {
+	layangan bambu;
+	
+	bambu.jenis ="kuning";
+	bambu.harga = 50000;
+	
+	cout<<"jenis yang dipakai "<< (bambu.jenis)<<endl;
+	cout<<"Harga modal nya "<< (bambu.harga)<<endl;
+	
+	bambu.bahan("bambu terbuat dari pohonnya sendiri");
+}
